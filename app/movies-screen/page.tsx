c950897dc3c691457e294420b84e168db7319dcb
Arcturus🚀 <101168144+Arcturus1803@users.noreply.github.com>
2025-01-31 01:37:14 +0900

    Update page.tsx

--- app/movies-screen/page.tsx
+++ app/movies-screen/page.tsx
@@ -1,8 +1,15 @@
+"use client";
+import { useState } from "react";
+
 export default function MoviesScreen() {
 
     return (
-        <div>
-            <h1>영화 페이지</h1>
-        </div>
+        <main className="flex flex-col justify-center items-center space-y-6 bg-white">
+            {/* 광고 자리 */}
+            <div className="bg-[#D9D9D9] w-[calc(100vw-40px)] h-[120px] mt-[60px]">광고</div>
+
+            {/* 영화 개봉순 나열열 */}
+            <div className=" w-120 h-10 border border-3 border-red-500">영화 개봉 순</div>
+        </main>
     );
 }
