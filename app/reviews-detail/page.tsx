424e9a683c8cfe9b30dcaed6c6cd7007e30195d6
Arcturus🚀 <101168144+Arcturus1803@users.noreply.github.com>
2025-02-02 14:44:49 +0900

    리뷰 디테일 틀

---
+++ app/reviews-detail/page.tsx
@@ -0,0 +1,11 @@
+export default function ReviewsDetail() {
+    return (
+        <main>
+            {/* 리뷰 정보*/}
+            <div></div>
+
+             {/* 댓글*/}
+             <div></div>
+        </main>
+    )
+}
