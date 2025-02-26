54f3e656450269127c25823443a18d65c183b311
Arcturus🚀 <101168144+Arcturus1803@users.noreply.github.com>
2025-02-26 14:21:34 +0900

    Update page.tsx
    댓글창 제작
    
    Co-Authored-By: Odyssey <77610388+Odyssey409@users.noreply.github.com>

--- app/livechat/page.tsx
+++ app/livechat/page.tsx
@@ -1,7 +1,25 @@
 export default function liveChat() {
     return (
-        <main className="flex flex-col justify-center items-center w-full">
-            <h1>hello</h1>
+        <main className="flex flex-col justify-center items-center w-full relative">
+            <div className="w-[89%] bg-[#D9D9D9] my-5">
+                <form className="w-full h-[954px] border border-3 border-black">
+                    {/** 댓글보드 */}
+                    <div>
+
+                    </div>
+
+                    {/** 입력 창 */}
+                    <div className="w-full h-[60px] flex items-end space-x-4 my-2 px-3">
+                            <input
+                            type="text"
+                            className="w-full max-w-[90%] h-full border border-none rounded-[30px] text-[16px] bg-[#EDEDED]"
+                            />
+                            <button className="h-full lg:w-[8%] border rounded-[30px] bg-[#FFC107] lg:text-[24px] md:text-[15px]">
+                            입력
+                            </button>
+                        </div>
+                </form>
+            </div>
         </main>
     )
 }
