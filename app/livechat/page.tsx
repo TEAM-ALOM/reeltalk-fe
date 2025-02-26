0a6a2b432dbee7bdb047e1d9cd0555d8bce6954c
Arcturus🚀 <101168144+Arcturus1803@users.noreply.github.com>
2025-02-26 14:16:39 +0900

    Create page.tsx
    실시간 talk more 페이지 생성
    
    Co-Authored-By: Odyssey <77610388+Odyssey409@users.noreply.github.com>

---
+++ app/livechat/page.tsx
@@ -0,0 +1,7 @@
+export default function liveChat() {
+    return (
+        <main className="flex flex-col justify-center items-center w-full">
+            <h1>hello</h1>
+        </main>
+    )
+}
